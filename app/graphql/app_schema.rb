a61dfe4a0faa437674e32cc5a763a9684788d2e2
Zlatko Zahariev <zlatko.zahariev@gmail.com>
2019-02-04 03:51:51 +0200

    Initial GraphQL IDL schema

--- app/graphql/app_schema.rb
+++ app/graphql/app_schema.rb
@@ -1,4 +1,26 @@
-class AppSchema < GraphQL::Schema
-  mutation(Types::MutationType)
-  query(Types::QueryType)
-end
+schema_path = File.expand_path('schema.graphql', __dir__)
+
+type_hash = {
+  'Query' => {
+    'users' => ->(_ob, _args, _ctx) { User.all },
+  },
+  'User' => {
+    'id'=> ->(obj, _args, _ctx) { obj.id },
+    'posts' => ->(obj, _args, _ctx) { obj.posts }
+  },
+  'Post' => {
+      'id' => ->(obj, _args, _ctx) { obj.id },
+      'comments' => ->(obj, _args, _ctx) { obj.comments }
+  },
+  'Comments' => {
+      'id' => ->(obj, _args, _ctx) { obj.id },
+      'user' => ->(obj, _args, _ctx) { obj.user },
+      'message' => ->(obj, _args, _ctx) { obj.message },
+      'likes' => ->(obj, _args, _ctx) { obj.likes },
+  },
+  'Likes' => {
+      'user' => ->(obj, _args, _ctx) { obj.users },
+  },
+}
+
+AppSchema = GraphQL::Schema.from_definition( schema_path, default_resolve: type_hash )
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
 
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/your/endpoint"
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
 end
